3efec0e40a8a564fde9e81cc8b0c50c09e08028d
hwa <gyeonghwaa_@naver.com>
2025-10-08 23:48:36 +0900

    http 서버 만들기

---
+++ adv2/src/was/httpserver/HttpRequestHandler.java
@@ -0,0 +1,44 @@
+package was.httpserver;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import static util.MyLogger.log;
+
+public class HttpRequestHandler implements Runnable {
+
+    private final Socket socket;
+    private final ServletManager servletManager;
+
+    public HttpRequestHandler(Socket socket, ServletManager servletManager) {
+        this.socket = socket;
+        this.servletManager = servletManager;
+    }
+
+    @Override
+    public void run() {
+        try{
+            process(socket);
+        }catch (Exception e){
+            log(e);
+            e.printStackTrace();
+        }
+    }
+
+    private void process(Socket socket) throws IOException {
+        try(socket; BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            PrintWriter
+                    writer = new PrintWriter(socket.getOutputStream(),false,StandardCharsets.UTF_8);){
+            HttpRequest request = new HttpRequest(reader);
+            HttpResponse response = new HttpResponse(writer);
+
+            log("HTTP 요청: " + request);
+            servletManager.execute(request,response);
+            response.flush();
+            log("HTTP 응답 완료");
+
+
+        }
+    }
+}
---
+++ adv2/src/was/v4/HttpRequestHandlerV4.java
@@ -0,0 +1,139 @@
+package was.v4;
+
+import was.httpserver.HttpRequest;
+import was.httpserver.HttpResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+import static util.MyLogger.log;
+
+public class HttpRequestHandlerV4 implements Runnable {
+    private final Socket socket;
+
+    public HttpRequestHandlerV4(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            process(socket);
+        } catch (Exception e) {
+            log(e);
+        }
+    }
+
+    private void process(Socket socket) throws IOException {
+        try (socket;
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), false, StandardCharsets.UTF_8);
+        ) {
+
+            HttpRequest request = new HttpRequest(reader);
+            HttpResponse response = new HttpResponse(writer);
+
+
+            if (request.getPath().contains("/favicon.ico")) {
+                log("favicon 요청");
+                return;
+            }
+
+            log("HTTP 요청 정보 출력");
+            System.out.println(request);
+
+            log("HTTP 응답 생성중...");
+            if (request.getPath().equals("/site1")) {
+                site1(response);
+            } else if (request.getPath().equals("/site2")) {
+                site2(response);
+            } else if (request.getPath().equals("/search")) {
+                search(request, response);
+            } else if (request.getPath().equals("/")) {
+                home(response);
+            } else {
+                notFound(response);
+            }
+            response.flush();
+            log("HTTP 응답 전달 완료");
+        }
+    }
+
+    private void notFound(HttpResponse response) {
+       response.setStatusCode(404);
+        response.writeBody("<h1>404 페이지를 찾을 수 없습니다.</h1>");
+
+    }
+
+    private void home(HttpResponse response) {
+        response.writeBody("<h1>home</h1>");
+        response.writeBody("<ul>");
+        response.writeBody("<li><a href='/site1'>site1</a></li>");
+        response.writeBody("<li><a href='/site2'>site2</a></li>");
+        response.writeBody("<li><a href='/search?q=hello'>검색</a></li>");
+        response.writeBody("</ul>");
+    }
+
+    private void search(HttpRequest request,HttpResponse response) {
+        String query = request.getParameter("q");
+        response.writeBody("<h1>Search</h1>");
+        response.writeBody("<ul>");
+        response.writeBody("<li>query: " + query + "</li>");
+        response.writeBody("</ul>");
+    }
+
+    private void site1(HttpResponse response) {
+        response.writeBody("<h1>site1</h1>");}
+
+    private void site2(HttpResponse response) {
+        response.writeBody("<h1>site2</h1>");
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void responseToClient(PrintWriter writer) {
+
+        String body = "<h1>Hello World</h1>";
+        int length = body.getBytes(StandardCharsets.UTF_8).length;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("HTTP/1.1 200 OK\r\n");
+        sb.append("Content-Type: text/html\r\n");
+        sb.append("Content-Length: ").append(length).append("\r\n");
+        sb.append("\r\n"); // header, body 구분 라인
+        sb.append(body);
+
+        log("HTTP 응답 정보 출력");
+        System.out.println(sb);
+
+        writer.println(sb);
+        writer.flush();
+
+    }
+
+    private String requestToString(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) {
+                break;
+            }
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
+
+
+}
